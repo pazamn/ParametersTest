eb0031bef0e67a86fd9ed3a68e59e69e451b03a8
pazamn <andrej_999@mail.ru>
2017-03-23 16:03:21 +0300

    + Initial commit

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace ParametersTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Executable Line:");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(Environment.CommandLine);
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Current Directory:");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(Environment.CurrentDirectory);
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Current User:");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(Environment.UserDomainName + "\\" + Environment.UserName);
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Executable arguments:");
+            if (!args.Any())
+            {
+                Console.WriteLine("\tNo arguments");
+            }
+
+            foreach (string arg in args)
+            {
+                int position = args.ToList().IndexOf(arg);
+                string positionValue = position.ToString(CultureInfo.InvariantCulture).PadLeft(args.Count().ToString(CultureInfo.InvariantCulture).Count());
+
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(positionValue + ": ");
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(arg);
+
+                Console.WriteLine();
+            }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
